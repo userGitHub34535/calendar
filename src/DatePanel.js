4cb9fe28eb7a4684c9b7ca8c29174c57387ab595
sheila <sheilasubbiah3@gmail.com>
2022-09-14 20:55:24 -0500

    remove all startup bloat

---
+++ src/DatePanel.js
@@ -0,0 +1,9 @@
+import './DatePanel.css';
+
+export default function DatePanel() {
+  return (
+    <div className="DatePanel">
+      <input type="time"/>
+    </div>
+  );
+}
--- src/DatePanel.test.js
+++ src/DatePanel.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import DatePanel from './DatePanel';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<DatePanel />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
