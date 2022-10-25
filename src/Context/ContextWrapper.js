ffe7a5688afb1eada9a8117d54bd0452815a0ce9
Sheila S <sheilasubbiah3@gmail.com>
2022-10-24 21:10:59 -0500

    some savedTLs updates

--- src/Context/ContextWrapper.js
+++ src/Context/ContextWrapper.js
@@ -1,4 +1,5 @@
 import { useState, createContext, useEffect } from "react";
+import axios from "axios";
 // import GlobalContext from "./GlobalContext";
 
 export const GlobalContext = createContext({
@@ -11,11 +12,23 @@ export const GlobalContext = createContext({
 export function ContextWrapper(props) {
     const [showTLForm, setShowTLForm] = useState(false);
     const [savedTLs, setSavedTLs] = useState([]);
+    const API = "http://localhost:8080/timeLabored";
 
     useEffect(() => {
+        //I wanted to save TLs to the DB here
+        alert("maybe useEffect fires for Context was loaded?, as opposed to savedTLs updated");
+        //I think this fires in Esteba because savedTL's setter function is a useReducer & a dispatcher. Let me research useReducer.  
+        //I really do think I have my answer - it's because savedTLs state in my ContextWrapper is = useState([]).
+        // However, in Esteban it actually = useReducer(...dispatcher...)
+        //so I think in mine, maybe the Context's savedTLs isn't updating, but if I were to useReducer & dispatcher, my state in the context, might update. 
         console.log(savedTLs);
         alert("SS");
     }, [savedTLs]);
+
+    useEffect(() => { 
+        alert("setShowTLForm context. state changed")
+    }, [showTLForm]);
+
 
     return (
         <GlobalContext.Provider value={
